8c45569fc615c3be485874a446ca863196d216e7
Krislucas <kaadan7@gmail.com>
2022-06-30 13:24:16 +0530

    Fixed broken Links of /customer & /blogs

--- src/components/BlogPage/header/header.jsx
+++ src/components/BlogPage/header/header.jsx
@@ -2,6 +2,7 @@ import Navbar from 'react-bootstrap/Navbar';
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import {Link} from "react-router-dom";
+import "./header.css";
 
 let Header = ()=>{
     return(
@@ -9,8 +10,10 @@ let Header = ()=>{
   <Container>
   <Navbar.Brand href="/">Welcome Lizma</Navbar.Brand>
   <Nav className="mx-100">
-        <Nav.Link href="/customer">Customer Table</Nav.Link>
-        <Nav.Link href="/blogs">Blogs .</Nav.Link>
+        {/* <Nav.Link href="/customer">Customer Table</Nav.Link>
+        <Nav.Link href="/blogs">Blogs .</Nav.Link> */}
+       <Nav.Link><Link to="/customer" className="Link">Customer Table</Link></Nav.Link>
+       <Nav.Link> <Link to="/blogs" className="Link">Blogs .</Link></Nav.Link>
     </Nav>
   
   </Container> 
